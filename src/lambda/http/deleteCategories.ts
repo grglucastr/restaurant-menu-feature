6a6aa9f66b30f9291a75d46ac8732e10037e9f00
George Bentes <george.bentes@gmail.com>
2020-04-29 16:47:38 -0300

    Add functions to categories

---
+++ src/lambda/http/deleteCategories.ts
@@ -0,0 +1,5 @@
+import { APIGatewayProxyResult, APIGatewayProxyHandler, APIGatewayProxyEvent } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  return null;
+}
---
+++ src/lambda/http/getCategories.ts
@@ -0,0 +1,5 @@
+import {APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  return null;
+}
