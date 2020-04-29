afcec905eac58f5e8ac216c456ee24e84259a5aa
George Bentes <george.bentes@gmail.com>
2020-04-29 16:53:02 -0300

    Avoid error due unused parameter

--- src/lambda/http/getCategories.ts
+++ src/lambda/http/getCategories.ts
@@ -1,5 +1,6 @@
 import {APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log(event);
   return null;
 }
