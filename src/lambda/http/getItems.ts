4d131d91115f8a2b41a2b1016cf2194e85c65bff
George Bentes <george.bentes@gmail.com>
2020-04-29 16:49:21 -0300

    Add functions items

---
+++ src/lambda/http/getItems.ts
@@ -0,0 +1,5 @@
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+export const handler:APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) : Promise<APIGatewayProxyResult> => {
+  return null;
+}
