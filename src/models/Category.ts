289dd16900fa7d1c7919f7312521bab9d052ba56
George Bentes <george.bentes@gmail.com>
2020-04-30 10:51:04 -0300

    Add models interface

---
+++ src/models/Category.ts
@@ -0,0 +1,6 @@
+export interface Category{
+  restaurantId: string
+  createdAt: string
+  categoryId: string
+  name:string
+}
---
+++ src/models/Item.ts
@@ -0,0 +1,9 @@
+export interface Item {
+  restaurantId: string
+  categoryId: string
+  itemId: string
+  createdAt: string
+  name: string
+  price: string
+  photo?: string
+}
